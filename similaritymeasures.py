323e4b5c0eafe339ad0684fe3ff5a8560b09c841
chidanandpujar <46497833+chidanandpujar@users.noreply.github.com>
2024-07-03 11:00:57 +0530

    Update and rename similarity_measures.py to similaritymeasures.py

---
+++ similaritymeasures.py
@@ -0,0 +1,21 @@
+"""Similariy measures 
+Euclidean distance
+Manhantten distance
+Minkowski distance
+Cosine similarity
+Jaccard similarity """
+#! /usr/bin/env python
+from math import *
+from decimal import Decimal
+
+Class Similarity():
+    """Five similarity measures function"""
+    def euclidean_distance(self, x, y):
+    def manhatan_distance(Self, x, y):
+    def minkowski_distance(self, x, y, p_value):
+    def nth_root(self, value, n_root):
+    def cosine_similarity(self, x, y):
+    def sqaure_rooted(self, x):
+    def jaccard_similarity(self, x, y)
+    
+
