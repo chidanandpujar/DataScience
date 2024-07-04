b2d0813d83cf459c482896b854e92be6729cf6a5
chidanandpujar <46497833+chidanandpujar@users.noreply.github.com>
2024-07-04 11:52:09 +0530

    Create numpy_test.py

---
+++ numpy_test.py
@@ -0,0 +1,14 @@
+import numpy as np
+# To create matrix of 2X2
+A = np.array[(1,2), (3,4])
+print(A)
+
+# To create an Identity matrix of 2x2
+B = np.identity(2)
+print(B)
+
+# To create matrix of random numbers 
+C = np.random.randn(2)
+print(C)
+
+# 
